b28d69c32abb549f92e371c13e9e6b1ef3bc4764
Quinn <91642013+qfetrow@users.noreply.github.com>
2022-01-13 13:20:24 -0800

    Initial Commit

---
+++ OregonBigTree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OregonBigTree
+//
+//  Created by Quinn Fetrow on 1/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OregonBigTree/OregonBigTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OregonBigTreeApp.swift
+//  OregonBigTree
+//
+//  Created by Quinn Fetrow on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct OregonBigTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
